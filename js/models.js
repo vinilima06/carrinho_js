0fcc51e79c5b581cc22d310164c3cce470dc7d0b
vinilima06 <130664885+vinilima06@users.noreply.github.com>
2024-02-29 08:11:17 -0300

    c

--- js/models.js
+++ js/models.js
@@ -1,1 +1,75 @@
-// Digitar aqui o código do models.js
+let modelsJson = [
+    // objeto1 
+    {
+        id: 1,
+        name: 'F4F Wildcat.png',
+        img: 'img/F4FWildcat.png',
+        price: [100.00, 150.00, 200.00],
+        sizes: ['1/72', '1/48', '1/32'],
+        description: 'Caça utilizado nos primeiros anos de combate no pacífico pela marinha dos EUA'
+    },
+    // objeto2
+    {
+        id: 2,
+        name: 'Focke Wulf Fw190 A5',
+        img: 'img/Fw190A5.png',
+        price: [80.00, 150.00,  200.00],
+        sizes: ['1/144', '1/48', '1/32'],
+        description: 'Caça de motor em estrela da Lufwaffe, temido em média e baixas altitudes'
+    },
+    // objeto3
+    {
+        id: 3,
+        name: 'Ki 61 - Hien',
+        img: 'img/Ki61Hien.png',
+        price: [100.00, 150.00,  200.00],
+        sizes: ['1/72', '1/48', '1/32'],
+        description: 'Caça fabricado pela Kawasaki para o exercito japonês, com motor em linha'
+    },
+    // objetivo 4
+    {
+        id: 4,
+        name: 'Me 109 G6',
+        img: 'img/Me109G6.png',
+        price: [100.00, 150.00,  200.00],
+        sizes: ['1/72', '1/48', '1/32'],
+        description: 'Famoso caça da Messerschimitt, voado pelos maiores ases da Lufwaffe, a versão G foi a mais produzida'   
+    },
+    // Objeto 5
+    {
+        id: 5,
+        name: 'P47 D - Thunderbold',
+        img: 'img/P47D.png',
+        price: [100.00, 150.00, 200.00],
+        sizes: ['1/72', '1/48', '1/32'],
+        description: 'Caça bombardeio de fabricação americana utilizado pela FAB'
+    },
+    // Objeto 6
+    {
+        id: 6,
+        name: 'P51 D - Mustang',
+        img: 'img/P51D.png',
+        price: [100.00, 150.00, 200.00],
+        sizes: ['1/72', '1/48', '1/32'],
+        description: 'O mais conhecido caça americano, importante escolta para os bombardeiros diurnos'
+    },
+    // Objeto 7
+    {
+        id: 7,
+        name: 'Spitfire Mk-IX',
+        img: 'img/SpitMk9.png',
+        price: [100.00, 150.00, 200.00],
+        sizes: ['1/72', '1/48', '1/32'],
+        description: 'O mais famoso caça britânico, utilizado em toda a guerra'
+    },
+    // Objeto 8
+    {
+        id: 8,
+        name: 'A6M Zero',
+        img: 'img/Zero.png',
+        price: [100.00, 150.00, 200.00],
+        sizes: ['1/72', '1/48', '1/32'],
+        description: 'O mais famoso caça da marinha japonesa, terror do pacífico nos anos iniciais da guerra'
+    }
+
+];
